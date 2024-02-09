4a0fe31da29f8d2842b98df1b60a26fccbfa8d70
JudithvdP <judith.v.d.p@hotmail.com>
2024-02-09 16:47:59 +0100

    First commit

---
+++ src/main/java/com/example/DMC_Project/DmcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.DMC_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmcProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DMC_Project/DmcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DMC_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
